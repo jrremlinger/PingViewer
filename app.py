7fe0b64a9cd63a5149ea51c61ddcc54731bc709d
Joshua <pickichu@icloud.com>
2022-03-30 16:17:20 +0000

    Added filtering, and fixed a truncation error.

--- app.py
+++ app.py
@@ -20,7 +20,9 @@ def tcpLoop():
 	p = sub.Popen(['sudo', 'tcpdump', '-nn', '(icmp[0] == 8) || (icmp6 && ip6[40] == 0x80)', '-U', '-l', '-t'], stdout=sub.PIPE)
 	for line in iter(p.stdout.readline, b''):
 		line = line.decode('utf-8').split()
-		data = '{"ip":"' + line[1] + '", "type":"' + line[4] + '", "id":"' + line[8][:-1] + '", "seq":"' + line[10][:-1] + '", "len":"' + line[12][:-1] + '"}'
+		if (line[4] == 'ICMP6,'):
+			line[4] = 'ICMP6'
+		data = '{"ip":"' + line[1] + '", "type":"' + line[4] + '", "id":"' + line[8][:-1] + '", "seq":"' + line[10][:-1] + '", "len":"' + line[12] + '"}'
 		testOut(data)
 		
 eventlet.spawn(tcpLoop)
--- static/script.js
+++ static/script.js
@@ -8,6 +8,62 @@ function writeData(data) {
 	dataArr[max] = data;
 	$('#dataLog').html(`<tr id='data_${max}'><td>${data.ip}</td><td>${data.type}</td><td>${data.id}</td><td>${data.seq}</td><td>${data.len}</td></tr>` + $('#dataLog').html());
 	max++;
+	updateFilter();
 }
+
+function updateFilter() {
+	if ($('#ipFilterCheck').prop('checked')) {
+		for (i in dataArr) {
+			if (!dataArr[i].ip.includes(($('#ipFilter').val()))) {
+				$('#data_' + i).hide();
+			} else {
+				filterProtocol(i);
+			}
+		}
+	} else {
+		for (i in dataArr) {
+			filterProtocol(i);
+		}
+	}
+}
+
+function filterProtocol(index) {
+	if (dataArr[index].type == 'ICMP' && $('#ipv4Check').prop('checked')) {
+		filterLength(index);
+	} else if (dataArr[index].type == 'ICMP' && !$('#ipv4Check').prop('checked')) {
+		$('#data_' + index).hide();
+	} else if (dataArr[index].type == 'ICMP6' && $('#ipv6Check').prop('checked')) {
+		filterLength(index);
+	} else if (dataArr[index].type == 'ICMP6' && !$('#ipv6Check').prop('checked')) {
+		$('#data_' + index).hide();
+	}
+}
+
+function filterLength(index) {
+	if ($('#lengthFilterCheck').prop('checked')) {
+		if ($('#filterOperator').val() == "equals") {
+			if (dataArr[index].len == $('#lengthFilter').val()) {
+				$('#data_' + index).show();
+			} else {
+				$('#data_' + index).hide();
+			}
+		} else if ($('#filterOperator').val() == "greater") {
+			if (dataArr[index].len > $('#lengthFilter').val()) {
+				$('#data_' + index).show();
+			} else {
+				$('#data_' + index).hide();
+			}
+		} else if ($('#filterOperator').val() == "less") {
+			if (dataArr[index].len < $('#lengthFilter').val()) {
+				$('#data_' + index).show();
+			} else {
+				$('#data_' + index).hide();
+			}
+		}
+	} else {
+		$('#data_' + index).show();
+	}
+}
+
 
 socket.on('send data', function(data) { writeData(JSON.parse(data)); });
